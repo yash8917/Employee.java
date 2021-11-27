0f7a389ca617254b1f099dc5454bb93792e17cd6
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-27 19:56:12 +0530

    Update TestArray.java

--- src/TestArray.java
+++ src/TestArray.java
@@ -3,7 +3,6 @@ public class TestArray {
     public static void main(String[] args) {
 //       int arr[];
        int arr[]=new int[3];
-        
        arr[0]=23;
        arr[1]=34;
        arr[2]=878;
