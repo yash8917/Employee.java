981bb4283d35131a0ac4c492bb35f1bb8211242e
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2023-06-16 23:30:29 +0530

    Initial Commit

--- src/Sorting/Selection_Sort.java
+++ src/Sorting/Selection_Sort.java
@@ -1,4 +1,34 @@
 package Sorting;
 
+import java.util.Scanner;
+
 public class Selection_Sort {
+
+    public static void selectionSort(int[] ar){
+        for (int i = 0; i < ar.length -1; i++) {
+            int smallest = i;
+            for (int j = i+1; j < ar.length ; j++) {
+                if (ar[smallest] > ar[j]){
+                    smallest =j;
+                }
+            }
+            int temp =ar[smallest];
+            ar[smallest]=ar[i];
+            ar[i]=temp;
+
+        }
+        for (int e: ar) {
+            System.out.print(e+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i]=sc.nextInt();
+        }
+        selectionSort(ar);
+    }
 }
